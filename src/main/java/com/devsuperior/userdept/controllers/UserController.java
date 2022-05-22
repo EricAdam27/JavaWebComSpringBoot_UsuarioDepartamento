22929a144fcc31d947206b4db6ea179f6acae24f
Eric Adam <eadamfs.programador@gmail.com>
2022-05-22 18:19:08 -0300

    Criado o endpoint findAllUser para buscar todos os usuários no banco

--- src/main/java/com/devsuperior/userdept/controllers/UserController.java
+++ src/main/java/com/devsuperior/userdept/controllers/UserController.java
@@ -1,9 +1,13 @@
 package com.devsuperior.userdept.controllers;
 
+import com.devsuperior.userdept.entities.User;
 import com.devsuperior.userdept.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -11,4 +15,9 @@ public class UserController {
 
     @Autowired
     private UserRepository repository;
+
+    @GetMapping
+    public List<User> findAllUser() {
+        return repository.findAll();
+    }
 }
